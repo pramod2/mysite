60ae10fc359420bf1093bdd2cf28840934feabc8
Pramod Kandel <kandelpramod2@gmail.com>
2016-06-14 22:52:35 -0700

    removed tinymce-filebrowser reference for now because it didn't work in production

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from blog.models import Post, Category
 from django.utils import timezone
-from mce_filebrowser.admin import MCEFilebrowserAdmin
 
 def publish_post(modeladmin, request, queryset):
     queryset.update(published_date=timezone.now())
@@ -9,10 +8,7 @@ def publish_post(modeladmin, request, queryset):
 
 publish_post.short_description = "Mark selected posts as published"
 
-class PostAdmin(MCEFilebrowserAdmin):
-    class Media:
-        js=('/static/tiny_mce/tiny_mce.js', '/static/tiny_mce/textarea_advanced.js',)
-
+class PostAdmin(admin.ModelAdmin):
     exclude = ['posted_date']
     prepopulated_fields = {"slug": ("title",)}
     list_display = ('title', 'created_date', 'last_updated', 'is_published')
--- mysite/urls.py
+++ mysite/urls.py
@@ -23,6 +23,5 @@ urlpatterns = [
     url(r'^blog/', include('blog.urls')),
     url(r'^tinymce/', include('tinymce.urls')),
     url(r'^grappelli/', include('grappelli.urls')), # grappelli URLS
-    url(r'^mce_filebrowser/', include('mce_filebrowser.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
